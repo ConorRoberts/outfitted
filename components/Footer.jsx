ada00f72956325070bf0bd9f0a14f3d73fc19703
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2021-04-05 14:02:43 -0400

    Footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,35 +1,56 @@
 import React from "react";
 import styles from "@styles/Footer.module.scss";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaFacebookF,
+  FaTwitter,
+  FaPhone,
+} from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.socials}>
-        <ul>
-          <li>
-            <a>
-              <FaInstagram />
-            </a>
-          </li>
-          <li>
-            <a>
-              <FaLinkedin />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className={styles.socials}>
+        <li>
+          <a href="https://www.instagram.com/theoutfittedapp/">
+            <FaInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/theoutfittedapp/">
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/TheOutfittedApp">
+            <FaTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/Theoutfittedapp-107446107674941">
+            <FaFacebookF />
+          </a>
+        </li>
+      </ul>
 
       <div>
-        <span className={styles.label}>
+        <span className={styles.icon}>
           <IoMdMail />
-          Email Us:{" "}
         </span>
-        theoutfittedapp@gmail.com
+        <p>
+          <span className={styles.label}>Email Us: </span>
+          theoutfittedapp@gmail.com
+        </p>
       </div>
       <div>
-        <span className={styles.label}>Tel: </span>905-718-3845
+        <span className={styles.icon}>
+          <FaPhone />
+        </span>
+        <p>
+          <span className={styles.label}>Tel: </span>905-718-3845
+        </p>
       </div>
     </div>
   );
